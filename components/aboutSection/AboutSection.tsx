54d1e1b05118595978878a7df76b78fb14e9e5b0
Lucas Ribeiro <lucas.rn00@gmail.com>
2022-10-10 13:21:03 -0300

    Make download CV button works

--- components/aboutSection/AboutSection.tsx
+++ components/aboutSection/AboutSection.tsx
@@ -14,50 +14,56 @@ import {
   StyledSpeechBubble,
 } from "./aboutSectionStyles";
 
-const AboutSection = () => (
-  <StyledSectionCustom id="about">
-    <StyledContainerCustom>
-      <SectionTitle>About Me</SectionTitle>
-      <StyledInformation>
-        <div>
-          <Image src={avatarTwo} alt="about me" />
-        </div>
-        <StyledSpeechBubble>
-          <StyledDescription role="paragraph">
-            I am Lucas Ribeiro Nunes, a web developer from São Bernardo do
-            Campo, Brazil. I have experience in front-end development with
-            ReactJs and NextJs
-          </StyledDescription>
-          <StyledSkillsWrapper>
-            <Skill
-              name="ReactJs/NextJs"
-              percentage={75}
-              barColor="#ffcf48"
-              barBgColor="#f2f0f4"
-            />
-            <Skill
-              name="HTML"
-              percentage={70}
-              barColor="#ff6d5c"
-              barBgColor="#f2f0f4"
-            />
-            <Skill
-              name="CSS"
-              percentage={65}
-              barColor="#6a72f7"
-              barBgColor="#f2f0f4"
-            />
-          </StyledSkillsWrapper>
-          <StyledButton type="button">Download CV</StyledButton>
-        </StyledSpeechBubble>
-      </StyledInformation>
-      <StyledBadges>
-        <Badge name="Projects Completed" number="198" iconClass="icon-fire" />
-        <Badge name="Cups of Coffee" number="5670" iconClass="icon-cup" />
-        <Badge name="Github Stars" number="0" iconClass="icon-star" />
-      </StyledBadges>
-    </StyledContainerCustom>
-  </StyledSectionCustom>
-);
-
+const AboutSection = () => {
+  const openCurriculum = () => {
+    window.open("/curriculum-lucas-ribeiro.pdf");
+  };
+  return (
+    <StyledSectionCustom id="about">
+      <StyledContainerCustom>
+        <SectionTitle>About Me</SectionTitle>
+        <StyledInformation>
+          <div>
+            <Image src={avatarTwo} alt="about me" />
+          </div>
+          <StyledSpeechBubble>
+            <StyledDescription role="paragraph">
+              I am Lucas Ribeiro Nunes, a web developer from São Bernardo do
+              Campo, Brazil. I have experience in front-end development with
+              ReactJs and NextJs
+            </StyledDescription>
+            <StyledSkillsWrapper>
+              <Skill
+                name="ReactJs/NextJs"
+                percentage={75}
+                barColor="#ffcf48"
+                barBgColor="#f2f0f4"
+              />
+              <Skill
+                name="HTML"
+                percentage={70}
+                barColor="#ff6d5c"
+                barBgColor="#f2f0f4"
+              />
+              <Skill
+                name="CSS"
+                percentage={65}
+                barColor="#6a72f7"
+                barBgColor="#f2f0f4"
+              />
+            </StyledSkillsWrapper>
+            <StyledButton onClick={openCurriculum} type="button">
+              Download CV
+            </StyledButton>
+          </StyledSpeechBubble>
+        </StyledInformation>
+        <StyledBadges>
+          <Badge name="Projects Completed" number="198" iconClass="icon-fire" />
+          <Badge name="Cups of Coffee" number="5670" iconClass="icon-cup" />
+          <Badge name="Github Stars" number="0" iconClass="icon-star" />
+        </StyledBadges>
+      </StyledContainerCustom>
+    </StyledSectionCustom>
+  );
+};
 export default AboutSection;
